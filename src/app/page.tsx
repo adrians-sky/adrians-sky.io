442182952eb5ac151587f45b26461a23bed5db17
Adrian Curammeng <adrianvcurammeng@gmail.com>
2025-04-13 15:20:41 -0400

    ui: added linkedin and github links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,44 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+
 const Home = () => {
-	return (
-		<div 
-			className="h-full flex justify-center 
-			items-center bg-rosepine-bg">
-			<span className="font-mono text-rosepine-text">
-				~ currently a work in progress ~
-			</span>
-		</div>
-	);
+    return (
+        <div 
+            className="
+                h-full 
+                flex 
+                flex-col 
+                justify-center 
+                items-center 
+                bg-rosepine-bg">
+            <p className="font-mono text-rosepine-text">
+                ~ currently a work in progress ~
+            </p>
+            <p className="font-mono text-rosepine-text">
+                View my {" "}
+                <a 
+                    className="inline-flex items-center gap-1 underline" 
+                    href="https://www.linkedin.com/in/adrians-sky"
+                    target="_blank">
+                    LinkedIn
+                    <FontAwesomeIcon 
+                        className="w-3 h-3"
+                        icon={faArrowUpRightFromSquare} /> 
+                </a>
+                {" "} or {" "}
+                <a 
+                    className="inline-flex items-center gap-1 underline" 
+                    href="https://github.com/adrians-sky"
+                    target="_blank">
+                    GitHub
+                    <FontAwesomeIcon 
+                        className="w-3 h-3"
+                        icon={faArrowUpRightFromSquare} /> 
+                </a>
+                {" "} page.
+            </p>
+        </div>
+    );
 };
 
 export default Home;
