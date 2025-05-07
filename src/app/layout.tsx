ad2b62b02a13b3309e4d3170172a3c9d589f47a3
Adrian Curammeng <adrianvcurammeng@gmail.com>
2025-05-07 17:36:05 -0400

    meta: added metatags for the home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,27 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "adrians-sky.io",
-  description: "A personal website built with TypeScript, Next.js, and TailwindCSS.",
-};
+  title: "Home | adrians-sky",
+  description: "A personal website for Adrian Curammeng built with Typescript, Next.js, and TailwindCSS, deployed on Vercel.",
+  keywords: ["Adrian Curammeng", "adrians-sky", "Website", "Portfolio", 
+             "Developer", "Personal Website", "TypeScript", "Next.js", 
+             "React", "Tailwind", "Vercel", "GitHub"],
+  author: "Adrian Curammeng",
+  creator: "Adrian Curammeng",
+  robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+          index: true,
+          follow: true,
+      },
+  },
+}
+
+export const viewport: Viewport = {
+    themeColor: "dark purple",
+    colorScheme: "dark",
+}
 
 export default function RootLayout({
   children,
