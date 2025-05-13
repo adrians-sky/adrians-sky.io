30fdbe3845332cadd3cd3ebcab5bf17a260ea8aa
Adrian Curammeng <adrianvcurammeng@gmail.com>
2025-05-13 18:12:46 -0400

    page: changed home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
+import "@/styles/globals.css"
 
-export const metadata: Metadata = {
-  title: "Home | adrians-sky",
-  description: "A personal website for Adrian Curammeng built with Typescript, Next.js, and TailwindCSS, deployed on Vercel.",
-  keywords: ["Adrian Curammeng", "adrians-sky", "Website", "Portfolio", 
-             "Developer", "Personal Website", "TypeScript", "Next.js", 
-             "React", "Tailwind", "Vercel", "GitHub"],
-  author: "Adrian Curammeng",
-  creator: "Adrian Curammeng",
-  robots: {
-      index: true,
-      follow: true,
-      googleBot: {
-          index: true,
-          follow: true,
-      },
-  },
+const HomeLayout = ({ children, }: { children: React.ReactNode }) => {
+    return (
+        <html lang="en">
+            <body>
+                <div className="min-h-screen bg-rosepine-bg">
+                    <NavBar />
+                    {children}
+                    <Footer />
+                </div>
+            </body>
+        </html>
+    );
 }
 
-export const viewport: Viewport = {
-    themeColor: "dark purple",
-    colorScheme: "dark",
-}
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html className="h-full" lang="en">
-      <body className="h-full">{children}</body>
-    </html>
-  );
-}
+export default HomeLayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,9 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import type { NextPage } from "next";
 
-export default function Home() {
+const Home: NextPage = () => {
     return (
-        <div 
-            className="
-                h-full 
-                flex 
-                flex-col 
-                justify-center 
-                items-center 
-                bg-rosepine-bg">
-            <p className="font-mono text-rosepine-text">
-                ~ currently a work in progress ~
-            </p>
-            <p className="font-mono text-rosepine-text">
-                View my {" "}
-                <a 
-                    className="inline-flex items-center gap-1 underline" 
-                    href="https://www.linkedin.com/in/adrians-sky"
-                    target="_blank">
-                    LinkedIn
-                    <FontAwesomeIcon 
-                        className="w-3 h-3"
-                        icon={faArrowUpRightFromSquare} /> 
-                </a>
-                {" "} or {" "}
-                <a 
-                    className="inline-flex items-center gap-1 underline" 
-                    href="https://github.com/adrians-sky"
-                    target="_blank">
-                    GitHub
-                    <FontAwesomeIcon 
-                        className="w-3 h-3"
-                        icon={faArrowUpRightFromSquare} /> 
-                </a>
-                {" "} page.
-            </p>
-        </div>
+        <div></div>
     );
-};
+}
 
+export default Home;
