4589edf70347b15aa8af1ac84aa89800be14c1ca
Adrian Curammeng <adrianvcurammeng@gmail.com>
2025-05-14 21:26:11 -0400

    page: added components

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,14 +1,31 @@
 import type { NextPage } from "next";
+import type { BlogPost } from "@/blogs/posts";
+import { getPosts } from "@/blogs/posts";
+import BlogPreview from "@/components/blogPreview";
 
 const Blogs: NextPage = () => {
+    const blogs: BlogPost[] = getPosts();
+
     return (
-        <div className="w-full flex flex-col items-center justify-center">
+        <div 
+            className="w-1/2
+                       flex 
+                       flex-col 
+                       items-center 
+                       justify-center 
+                       mx-auto
+                       px-16">
             <h1 className="text-rosepine-text font-mono">blogs</h1>
             <p className="text-rosepine-muted italic font-mono">
                 a chaotic collection of my thoughts.
             </p>
+            <div className="w-full">
+                {blogs.map(blog => (
+                    <BlogPreview key={blog.id} post={blog} />
+                ))}
+            </div>
         </div>
     );
 }
 
-export default Blogs;;
+export default Blogs;
